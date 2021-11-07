0e8b9d92b3bb6f34a88264f127017284485b9a40
Cherviatsova <chervyatsovaelena2@gmail.com>
2021-11-07 19:08:10 +0200

    Выполнено ДЗ 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const storageValue = {
+  email: '',
+  message: '',
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.input.addEventListener('input', throttle(onEmailInput, 500));
+refs.textarea.addEventListener('input', throttle(onMessageInput, 500));
+
+function onEmailInput(evt) {
+  storageValue.email = evt.target.value;
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(storageValue));
+}
+
+function onMessageInput(evt) {
+  storageValue.message = evt.target.value;
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(storageValue));
+}
+
+function populateTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (savedMessage) {
+    refs.input.value = savedMessage.email;
+    refs.textarea.value = savedMessage.message;
+  }
+}
+populateTextarea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+
+  if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+    console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+  }
+
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
