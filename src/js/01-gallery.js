828f48cacee4db4d373a1c8130a9992990c9065e
Cherviatsova <chervyatsovaelena2@gmail.com>
2021-11-07 14:25:40 +0200

    js test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,29 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
+const imageContainer = document.querySelector('.gallery');
+const imageMarkup = createImagesMarkup(galleryItems);
 
-const gallery = document.querySelector('.gallery');
-const galleryImages = createGalleryImagesMarkup(galleryItems);
-
-gallery.insertAdjacentHTML('beforeend', galleryImages);
-
-function createGalleryImagesMarkup(galleryItems) {
-  return galleryItems
+function createImagesMarkup(images) {
+  return images
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
-            <img class="gallery__image"
-                src="${preview}"
-                alt="${description}" />
-</a>`;
+      return `  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" data-title="${description}" />
+  </a>`;
     })
     .join('');
 }
-let lightbox = new SimpleLightbox('.gallery a', {
-  captions: true,
+imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
-  captionPosition: 'bottom',
   captionDelay: 250,
 });
+// Change code below this line
 
 console.log(galleryItems);
